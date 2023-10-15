f18cc6b9ab6548b8d5d03a0ee85139093dc9a4f5
Girish YR <145290264+GirishYR@users.noreply.github.com>
2023-10-15 22:59:19 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Girish YR>
    # Input:
    # p, principal amount
    # t, time period in years
